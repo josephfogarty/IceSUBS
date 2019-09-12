3b99a572394dd27dc09f055616e43a11fbbcadb2
Joseph Fogarty <36773696+jjf1218@users.noreply.github.com>
2019-09-12 09:59:58 -0400

    Create initial_air_temp_calc.py
    create a quick script to calculate the initial air temp based on fractions of the sea ice surface

---
+++ het_surface_creation/initial_air_temp_calc.py
@@ -0,0 +1,23 @@
+# A code to get a starting initial air temperature for an LES simulation
+
+# import needed modules
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+# load path for the template
+lp = os.path.join("ice_maps","without_ponds","beaufo_2000aug31a_3c.out")
+
+arr = np.loadtxt(lp)
+
+unique, counts = np.unique(arr, return_counts=True)
+per_dict = dict(zip(unique, counts/arr.size))
+
+# Define temperatures corresponding to surface type
+T_ice = 269.35 #Kelvin
+T_sea = 273.35 #Kelvin
+T_pond = 272.35 # Kelvin
+
+air_temp = T_ice*per_dict[1] + T_sea*per_dict[2]
+
+print(f"The initial air temp is {air_temp}")
